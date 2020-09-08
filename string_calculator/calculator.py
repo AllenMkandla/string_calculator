dd8f2a6ce6340d1c6c35754f1fb6b54336b60f89
Allen <allen.mkandla@umuzi.org>
2020-09-08 12:15:54 +0200

    final changes

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,41 +1,47 @@
 import re
 
-def add(string):
-    sum = 0
-
-    if len(string) == 0: 
+def add(number_str):
+    number_str = r'{}'.format(number_str)
+    if len(number_str) == 0:
         return 0
-    
-    if len(string) == 1:
-        return int(string)
-    
-    elif string[0] == "/":
-        result = 0 
-        delim = string[3]
+
+    elif len(number_str) == 1:
+        if len(number_str) == 1:
+            return int(number_str)
+
+    elif number_str[0] == "/":
+         result = 0
+         delim = number_str[3]
 
     else:
+        # extracting all numbers from the string input, both positive and negative
+        num_l = re.findall(r"-?\d+", number_str)
         result = 0
         delim = ","
-        if string[1]!=",":
+        if number_str[1] != ",":
             delim = "\n"
-            numbers = string.split(delim)
-
-    numbers = re.findall(r"\d+|-\d+",string)
-    negetive_numbers = []
-    positive_numbers = []
-    total = 0
-
-    for i, num in enumerate(numbers):
-        if int(num) < 0:
-            negetive_numbers.append(int(num))
-    
-        if int(num) > 0:
-            positive_numbers.append(int(num))
-
-            # if int(num) >= 1000:
-    for i, x in enumerate(positive_numbers):
-        if int(x) >1000:
-            positive_numbers[i] = 0
+        numbers = number_str.split(delim)
+        for num in numbers:
+            result += int(num)
+        print(num_l)
+        # extracting negative numbers from num_l
+        neg_nums = []
+        for num in num_l:
+            if int(num) < 0:
+                neg_nums.append(num)
+        # raise exception with negative numbers
+        if len(neg_nums) != 0:
+            exception_msg = 'ERROR: negatives not allowed '
+            for pos in range(len(neg_nums)):
+                if pos != len(neg_nums) - 1:
+                    exception_msg = exception_msg + neg_nums[pos] + ','
+                else:
+                    exception_msg = exception_msg + neg_nums[pos]
+            raise Exception(exception_msg)
+        for num in num_l:
+            if int(num) < 1000:
+                result = result + int(num)
+        return result
 
     if len(negetive_numbers) > 0:
         raise Exception('negatives not allowed: {}'.format(negetive_numbers))
@@ -43,7 +49,3 @@ def add(string):
         for i in positive_numbers:
             total += int(i)
     return total
-    
-
-
-    
--- string_calculator/calculator_test.py
+++ string_calculator/calculator_test.py
@@ -2,23 +2,43 @@ from calculator import add
 import pytest
 
 def test_empty_string():
- assert(add("")) == 0
+    assert(add("")) == 0
 
 def test_one_intiger():
      assert(add("1")) == 1
 
-    #two intigers
+#two intigers
 def test_two_intigers():
-    assert(add("1,1")) == 2S
+    assert(add("1,1")) == 2
 
     #multiple intigers,
 def test_multiple_intigers():
     assert(add("1,2,3,4")) == 10
 
     #handle new lines
-def test_handle_new_lines:
+def test_handle_new_lines():
     assert(add("1\n2,3")) == 6
 
+# handle different delimeters
+def test_first_case_delimeter_exception():
+    assert(add("//;\n1;2")) == 3
 
+def test_negative_num_exception():
+    with pytest.raises(Exception) as negative:
+        assert(negative.add("-1,-2,3,4")) == "ERROR: negatives not allowed -1,-2"
 
+def test_greater_than_1000():
+    assert(add("//;\n1000,1;2")) == 3
+
+def test_delimeters_of_any_length():
+    assert (add("//***\n1***2***3")) == 6
+
+def test_different_delimeters():
+    assert(add("//[:D][%]\n1:D2%3")) == 6
+
+def test_different_delimeters():
+    assert(add("//[***][%%%]\n1***2%%%3")) == 6
+
+def test_different_delimeters():
+   assert(add("//[(-_-')][%]\n1(-_-')2%3")) == 6
 
